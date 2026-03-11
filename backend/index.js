49975068baa67599b3ca73b99a73d320bacdbbe5
DiegoLagosBesoain <dnlagos@miuandes.cl>
2026-03-10 22:44:34 -0300

    preparando deploy2

--- backend/index.js
+++ backend/index.js
@@ -6,5 +6,5 @@ import app from "./app.js";
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  console.log(`Servidor escuchando en ${PORT}`);
 });
