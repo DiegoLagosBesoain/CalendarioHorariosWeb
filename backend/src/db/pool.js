1eece91c1858f23a046276cd3823d7444281b46f
DiegoLagosBesoain <dnlagos@miuandes.cl>
2026-03-30 23:39:44 -0300

    coneccion a supabase2

--- backend/src/db/pool.js
+++ backend/src/db/pool.js
@@ -1,22 +1,25 @@
 import pkg from "pg";
 import dotenv from "dotenv";
-import dns from "dns";
 
 dotenv.config();
-dns.setDefaultResultOrder("ipv4first"); // 👈 fuerza IPv4
 
 const { Pool } = pkg;
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  ssl: {
-    rejectUnauthorized: false, // 👈 obligatorio para Supabase
-  },
-});
+const isProduction = process.env.NODE_ENV === "production";
 
-export { pool };
+const pool = isProduction
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : new Pool({
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    });
+
 export default pool;
